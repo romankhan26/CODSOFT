d7c894030018901eda7133153bb954d775d56df1
roman khan <romankhan.26@outlook.com>
2024-05-18 21:47:34 +0500

    Made Simple Calculator

---
+++ Simple_Calculator Using HTML CSS n JS/main.js
@@ -0,0 +1,18 @@
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
+};
+
+function clearDisplay(){
+  display.value = "";
+};
+
+function calculate(){
+    try {
+        const result = new Function('return ' + display.value)();
+        display.value = result;
+    } catch (error) {
+        display.value = "Error";
+    }
+};
